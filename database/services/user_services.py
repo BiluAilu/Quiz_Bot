52354384b1b79fae8885e3f3c4aaa291f9701851
Bilkes Elias <67165306+BiluAilu@users.noreply.github.com>
2024-01-09 15:41:33 +0300

    banned user doesn't get services

--- database/services/user_services.py
+++ database/services/user_services.py
@@ -14,7 +14,6 @@ async def get_user_by_id(id: int) -> User or None:
     return User(**user) if user else None
 async def is_blocked(id: int) -> User or None:
     user = await users_collection.find_one({'_id': id})
-    print(user)
     return 0 if user['status'] else 1
     # return User(**user) if user else None
 
